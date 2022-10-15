693167658fa923b281a786cbcfa9721b38a55fcc
bharathaladandi <bharathaladandi@gmail.com>
2022-10-15 15:17:58 +0530

    Third Commit

---
+++ src/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express")
+const cors = require("cors");
+
+const connect = require("./config/db");
+
+const userRoute = require("./features/user/user.router");
+const productRoute = require("./features/product/product.router");
+const cartRoute = require("./features/cart/cart.router");
+
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.use("/users", userRoute)
+app.use("/producs", productRoute)
+app.use("/carts", cartRoute)
+
+
+
+
+app.listen(PORT, async () => {
+    await connect();
+    console.log(`Listening on http://localhost:${PORT}`);
+})
